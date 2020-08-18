6cfbdd506102c9dffebfc29645ba3c130ca6fa6a
yuanhuihai <43170402+yuanhuihai@users.noreply.github.com>
2020-08-18 22:01:46 +0800

    20200818

--- alimysql/Form1.cs
+++ alimysql/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using mySqlHelper;
 
 namespace alimysql
 {
@@ -15,6 +17,33 @@ namespace alimysql
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+         MysqlOperate  sqlOperate = new MysqlOperate();
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            MySqlConnection conn = sqlOperate.MySqlGetCon();
+            conn.Open();
+            string sql = "select * from xsl";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();//执行ExecuteReader()返回一个MySqlDataReader对象
+            while (reader.Read())//初始索引是-1，执行读取下一行数据，返回值是bool
+            {
+         
+                listBox1.Items.Add( reader.GetString("shijian") + reader.GetString("shuliang"));//"userid"是数据库对应的列名，推荐这种方式
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "insert into xsl values('"+textBox1.Text+"','"+textBox2.Text+"')";
+            sqlOperate.MySqlCom(sql);
         }
     }
 }
